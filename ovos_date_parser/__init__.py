fac0a3f98aefe868f16fd7b2b4d1f74634281281
miro <jarbasai@mailfence.com>
2024-11-07 01:33:24 +0000

    feat: nice_xx

--- ovos_date_parser/__init__.py
+++ ovos_date_parser/__init__.py
@@ -1,5 +1,11 @@
+import json
+import os
+import re
+from collections import namedtuple
 from datetime import datetime, timedelta, time
 from typing import Optional, Tuple, Union
+
+from ovos_utils.lang import standardize_lang_tag
 
 from ovos_date_parser.dates_az import (
     extract_datetime_az,
@@ -87,7 +93,7 @@ from ovos_date_parser.dates_uk import (
     extract_datetime_uk,
     extract_duration_uk,
     nice_time_uk,
-nice_duration_uk
+    nice_duration_uk
 )
 
 
@@ -286,3 +292,311 @@ def extract_datetime(
     if lang.startswith("uk"):
         return extract_datetime_uk(text, anchorDate=anchorDate, default_time=default_time)
     raise NotImplementedError(f"Unsupported language: {lang}")
+
+
+NUMBER_TUPLE = namedtuple(
+    'number',
+    ('x, xx, x0, x_in_x0, xxx, x00, x_in_x00, xx00, xx_in_xx00, x000, ' +
+     'x_in_x000, x0_in_x000, x_in_0x00'))
+
+
+class DateTimeFormat:
+    def __init__(self, config_path):
+        self.lang_config = {}
+        self.config_path = config_path
+
+    def cache(self, lang):
+        # TODO - find closest lang code
+        if lang not in self.lang_config:
+            try:
+                # Attempt to load the language-specific formatting data
+                with open(self.config_path + '/' + lang + '/date_time.json',
+                          'r', encoding='utf8') as lang_config_file:
+                    self.lang_config[lang] = json.loads(
+                        lang_config_file.read())
+            except FileNotFoundError:
+                # Fallback to English formatting
+                with open(self.config_path + '/en-us/date_time.json',
+                          'r') as lang_config_file:
+                    self.lang_config[lang] = json.loads(
+                        lang_config_file.read())
+
+            for x in ['decade_format', 'hundreds_format', 'thousand_format',
+                      'year_format']:
+                i = 1
+                while self.lang_config[lang][x].get(str(i)):
+                    self.lang_config[lang][x][str(i)]['re'] = (
+                        re.compile(self.lang_config[lang][x][str(i)]['match']
+                                   ))
+                    i = i + 1
+
+    def _number_strings(self, number, lang):
+        x = (self.lang_config[lang]['number'].get(str(number % 10)) or
+             str(number % 10))
+        xx = (self.lang_config[lang]['number'].get(str(number % 100)) or
+              str(number % 100))
+        x_in_x0 = self.lang_config[lang]['number'].get(
+            str(int(number % 100 / 10))) or str(int(number % 100 / 10))
+        x0 = (self.lang_config[lang]['number'].get(
+            str(int(number % 100 / 10) * 10)) or
+              str(int(number % 100 / 10) * 10))
+        xxx = (self.lang_config[lang]['number'].get(str(number % 1000)) or
+               str(number % 1000))
+        x00 = (self.lang_config[lang]['number'].get(str(int(
+            number % 1000 / 100) * 100)) or
+               str(int(number % 1000 / 100) * 100))
+        x_in_x00 = self.lang_config[lang]['number'].get(str(int(
+            number % 1000 / 100))) or str(int(number % 1000 / 100))
+        xx00 = self.lang_config[lang]['number'].get(str(int(
+            number % 10000 / 100) * 100)) or str(int(number % 10000 / 100) *
+                                                 100)
+        xx_in_xx00 = self.lang_config[lang]['number'].get(str(int(
+            number % 10000 / 100))) or str(int(number % 10000 / 100))
+        x000 = (self.lang_config[lang]['number'].get(str(int(
+            number % 10000 / 1000) * 1000)) or
+                str(int(number % 10000 / 1000) * 1000))
+        x_in_x000 = self.lang_config[lang]['number'].get(str(int(
+            number % 10000 / 1000))) or str(int(number % 10000 / 1000))
+        x0_in_x000 = self.lang_config[lang]['number'].get(str(int(
+            number % 10000 / 1000) * 10)) or str(int(number % 10000 / 1000) * 10)
+        x_in_0x00 = self.lang_config[lang]['number'].get(str(int(
+            number % 1000 / 100)) or str(int(number % 1000 / 100)))
+
+        return NUMBER_TUPLE(
+            x, xx, x0, x_in_x0, xxx, x00, x_in_x00, xx00, xx_in_xx00, x000,
+            x_in_x000, x0_in_x000, x_in_0x00)
+
+    def _format_string(self, number, format_section, lang):
+        s = self.lang_config[lang][format_section]['default']
+        i = 1
+        while self.lang_config[lang][format_section].get(str(i)):
+            e = self.lang_config[lang][format_section][str(i)]
+            if e['re'].match(str(number)):
+                return e['format']
+            i = i + 1
+        return s
+
+    def _decade_format(self, number, number_tuple, lang):
+        s = self._format_string(number % 100, 'decade_format', lang)
+        decade = s.format(x=number_tuple.x, xx=number_tuple.xx,
+                          x0=number_tuple.x0, x_in_x0=number_tuple.x_in_x0,
+                          number=str(number % 100))
+        return s.format(x=number_tuple.x, xx=number_tuple.xx,
+                        x0=number_tuple.x0, x_in_x0=number_tuple.x_in_x0,
+                        number=str(number % 100))
+
+    def _number_format_hundreds(self, number, number_tuple, lang,
+                                formatted_decade):
+        s = self._format_string(number % 1000, 'hundreds_format', lang)
+        hundreds = s.format(xxx=number_tuple.xxx, x00=number_tuple.x00,
+                            x_in_x00=number_tuple.x_in_x00,
+                            formatted_decade=formatted_decade,
+                            number=str(number % 1000))
+        return s.format(xxx=number_tuple.xxx, x00=number_tuple.x00,
+                        x_in_x00=number_tuple.x_in_x00,
+                        formatted_decade=formatted_decade,
+                        number=str(number % 1000))
+
+    def _number_format_thousand(self, number, number_tuple, lang,
+                                formatted_decade, formatted_hundreds):
+        s = self._format_string(number % 10000, 'thousand_format', lang)
+        return s.format(x_in_x00=number_tuple.x_in_x00,
+                        xx00=number_tuple.xx00,
+                        xx_in_xx00=number_tuple.xx_in_xx00,
+                        x000=number_tuple.x000,
+                        x_in_x000=number_tuple.x_in_x000,
+                        x0_in_x000=number_tuple.x0_in_x000,
+                        x_in_0x00=number_tuple.x_in_0x00,
+                        formatted_decade=formatted_decade,
+                        formatted_hundreds=formatted_hundreds,
+                        number=str(number % 10000))
+
+    def date_format(self, dt, lang, now):
+        format_str = 'date_full'
+        if now:
+            if dt.year == now.year:
+                format_str = 'date_full_no_year'
+                if dt.month == now.month and dt.day > now.day:
+                    format_str = 'date_full_no_year_month'
+
+            tomorrow = now + datetime.timedelta(days=1)
+            yesterday = now - datetime.timedelta(days=1)
+            if tomorrow.date() == dt.date():
+                format_str = 'tomorrow'
+            elif now.date() == dt.date():
+                format_str = 'today'
+            elif yesterday.date() == dt.date():
+                format_str = 'yesterday'
+
+        return self.lang_config[lang]['date_format'][format_str].format(
+            weekday=self.lang_config[lang]['weekday'][str(dt.weekday())],
+            month=self.lang_config[lang]['month'][str(dt.month)],
+            day=self.lang_config[lang]['date'][str(dt.day)],
+            formatted_year=self.year_format(dt, lang, False))
+
+    def date_time_format(self, dt, lang, now, use_24hour, use_ampm):
+        date_str = self.date_format(dt, lang, now)
+        time_str = nice_time(dt, lang, use_24hour=use_24hour,
+                             use_ampm=use_ampm)
+        return self.lang_config[lang]['date_time_format']['date_time'].format(
+            formatted_date=date_str, formatted_time=time_str)
+
+    def year_format(self, dt, lang, bc):
+        number_tuple = self._number_strings(dt.year, lang)
+        formatted_bc = (
+            self.lang_config[lang]['year_format']['bc'] if bc else '')
+        formatted_decade = self._decade_format(
+            dt.year, number_tuple, lang)
+        formatted_hundreds = self._number_format_hundreds(
+            dt.year, number_tuple, lang, formatted_decade)
+        formatted_thousand = self._number_format_thousand(
+            dt.year, number_tuple, lang, formatted_decade, formatted_hundreds)
+
+        s = self._format_string(dt.year, 'year_format', lang)
+        return re.sub(' +', ' ',
+                      s.format(
+                          year=str(dt.year),
+                          century=str(int(dt.year / 100)),
+                          decade=str(dt.year % 100),
+                          formatted_hundreds=formatted_hundreds,
+                          formatted_decade=formatted_decade,
+                          formatted_thousand=formatted_thousand,
+                          bc=formatted_bc)).strip()
+
+
+date_time_format = DateTimeFormat(os.path.join(os.path.dirname(__file__), 'res'))
+
+
+def nice_date(dt, lang, now=None):
+    """
+    Format a datetime to a pronounceable date
+
+    For example, generates 'tuesday, june the fifth, 2018'
+
+    Args:
+        dt (datetime): date to format (assumes already in local timezone)
+        lang (str, optional): an optional BCP-47 language code, if omitted
+                              the default language will be used.
+        now (datetime): Current date. If provided, the returned date for speech
+            will be shortened accordingly: No year is returned if now is in the
+            same year as td, no month is returned if now is in the same month
+            as td. If now and td is the same day, 'today' is returned.
+
+    Returns:
+        (str): The formatted date string
+    """
+    full_code = standardize_lang_tag(lang)
+    date_time_format.cache(full_code)
+
+    return date_time_format.date_format(dt, full_code, now)
+
+
+def nice_date_time(dt, lang, now=None, use_24hour=False,
+                   use_ampm=False):
+    """
+        Format a datetime to a pronounceable date and time
+
+        For example, generate 'tuesday, june the fifth, 2018 at five thirty'
+
+        Args:
+            dt (datetime): date to format (assumes already in local timezone)
+            lang (str, optional): an optional BCP-47 language code, if omitted
+                                  the default language will be used.
+            now (datetime): Current date. If provided, the returned date for
+                speech will be shortened accordingly: No year is returned if
+                now is in the same year as td, no month is returned if now is
+                in the same month as td. If now and td is the same day, 'today'
+                is returned.
+            use_24hour (bool): output in 24-hour/military or 12-hour format
+            use_ampm (bool): include the am/pm for 12-hour format
+        Returns:
+            (str): The formatted date time string
+    """
+
+    full_code = standardize_lang_tag(lang)
+    date_time_format.cache(full_code)
+
+    return date_time_format.date_time_format(dt, full_code, now, use_24hour,
+                                             use_ampm)
+
+
+def nice_day(dt, lang, date_format='DMY', include_month=True):
+    if include_month:
+        month = nice_month(dt, date_format, lang)
+        if date_format == 'MDY':
+            return "{} {}".format(month, dt.strftime("%d"))
+        else:
+            return "{} {}".format(dt.strftime("%d"), month)
+    return dt.strftime("%d")
+
+
+def nice_weekday(dt, lang):
+    full_code = standardize_lang_tag(lang)
+    date_time_format.cache(full_code)
+
+    if full_code in date_time_format.lang_config.keys():
+        localized_day_names = list(
+            date_time_format.lang_config[lang]['weekday'].values())
+        weekday = localized_day_names[dt.weekday()]
+    else:
+        weekday = dt.strftime("%A")
+    return weekday.capitalize()
+
+
+def nice_month(dt, lang, date_format='MDY'):
+    full_code = standardize_lang_tag(lang)
+    date_time_format.cache(full_code)
+
+    if full_code in date_time_format.lang_config.keys():
+        localized_month_names = date_time_format.lang_config[lang]['month']
+        month = localized_month_names[str(int(dt.strftime("%m")))]
+    else:
+        month = dt.strftime("%B")
+    return month.capitalize()
+
+
+def nice_year(dt, lang, bc=False):
+    """
+        Format a datetime to a pronounceable year
+
+        For example, generate 'nineteen-hundred and eighty-four' for year 1984
+
+        Args:
+            dt (datetime): date to format (assumes already in local timezone)
+            lang (str, optional): an optional BCP-47 language code, if omitted
+                                  the default language will be used.
+            bc (bool) pust B.C. after the year (python does not support dates
+                B.C. in datetime)
+        Returns:
+            (str): The formatted year string
+    """
+
+    full_code = standardize_lang_tag(lang)
+    date_time_format.cache(full_code)
+    return date_time_format.year_format(dt, full_code, bc)
+
+
+def get_date_strings(dt, lang, date_format='MDY', time_format="full"):
+    lang = standardize_lang_tag(lang)
+    timestr = nice_time(dt, lang, speech=False,
+                        use_24hour=time_format == "full")
+    monthstr = nice_month(dt, date_format, lang)
+    weekdaystr = nice_weekday(dt, lang)
+    yearstr = dt.strftime("%Y")
+    daystr = nice_day(dt, date_format, include_month=False, lang=lang)
+    if date_format == 'MDY':
+        dtstr = dt.strftime("%-m/%-d/%Y")
+    elif date_format == 'DMY':
+        dtstr = dt.strftime("%d/%-m/%-Y")
+    elif date_format == 'YMD':
+        dtstr = dt.strftime("%Y/%-m/%-d")
+    else:
+        raise ValueError("invalid date_format")
+    return {
+        "date_string": dtstr,
+        "time_string": timestr,
+        "month_string": monthstr,
+        "day_string": daystr,
+        'year_string': yearstr,
+        "weekday_string": weekdaystr
+    }

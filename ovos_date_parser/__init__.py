639817aa472338f635f6bbf76d0cd5465edff475
miro <jarbasai@mailfence.com>
2024-11-11 17:41:51 +0000

    fix:nice_relative_time_en

--- ovos_date_parser/__init__.py
+++ ovos_date_parser/__init__.py
@@ -35,6 +35,7 @@ from ovos_date_parser.dates_de import (
 from ovos_date_parser.dates_en import (
     extract_datetime_en,
     extract_duration_en,
+nice_relative_time_en,
     nice_time_en
 )
 from ovos_date_parser.dates_es import (
@@ -175,6 +176,8 @@ def nice_relative_time(when, relative_to, lang):
     Returns:
         str: Relative description of the given time
     """
+    if lang.startswith("en"):
+        return nice_relative_time_en(when, relative_to)
     if lang.startswith("eu"):
         return nice_relative_time_eu(when, relative_to)
     raise NotImplementedError(f"Unsupported language: {lang}")
